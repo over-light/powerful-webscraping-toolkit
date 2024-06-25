7a13a3372e9c0eddf2e81050e3cc6d22ddefff26
over-light <over.light888@gmail.com>
2024-06-25 17:03:16 +0900

    feat: add chromecontroller backbone

---
+++ chrmecontroller/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+using System.Windows.Threading;
+using System.Net;
+using System.Net.Http;
+
+namespace ChromeController
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {        public MainWindow()
+        {
+           
+           
+        }
+    }
+}
+
